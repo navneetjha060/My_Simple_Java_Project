249b5020611074411a633d33b6e9579ca37879b0
navneet <navneet.gullu@gmail.com>
2024-03-27 19:59:32 +0530

    First Commit to push things to GitHub

---
+++ src/My_Sample/First_Class.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package My_Sample;
+
+/**
+ * @author navneet
+ *
+ */
+public class First_Class {
+
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("This is my First Class");
+	}
+
+}
