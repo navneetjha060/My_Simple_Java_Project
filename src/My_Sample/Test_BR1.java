141f0a52251f04b16d56eae40ca9dff00bc8d123
navneet <navneet.gullu@gmail.com>
2024-03-27 20:07:44 +0530

    This is test class for br1

---
+++ src/My_Sample/Test_BR1.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package My_Sample;
+
+/**
+ * @author navneet
+ * This is code for test branch br1
+ *
+ */
+public class Test_BR1 {
+
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("This is branch One");
+
+	}
+
+}
